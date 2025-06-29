02a19893a1ae39d893882ca35bf476c6e8fe8289
msqueen62 <ff241012@gmail.com>
2025-06-29 22:28:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+import os
+import json
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+
+# Ambil JSON kredensial dari ENV
+json_str = os.environ.get("GOOGLE_CREDENTIALS_JSON")
+
+# Parsing string ke dict
+creds_dict = json.loads(json_str)
+
+# Gunakan dict sebagai kredensial
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+client = gspread.authorize(creds)
