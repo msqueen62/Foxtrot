47b58053daecbbdd2ec90c517d09a9714a0aeb79
msqueen62 <ff241012@gmail.com>
2025-06-29 22:55:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import os
 import json
+import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+# Scope akses untuk Google Sheets dan Drive
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
 
 # Ambil JSON kredensial dari ENV
-json_str = os.environ.get("GOOGLE_CREDENTIALS_JSON")
+json_str = os.environ.get("GOOGLE_CREDS")
+
+# Validasi: kalau kosong, langsung berhenti
+if not json_str:
+    raise Exception("Environment variable GOOGLE_CREDS tidak ditemukan!")
 
 # Parsing string ke dict
 creds_dict = json.loads(json_str)
 
 # Gunakan dict sebagai kredensial
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
+# Authorize dengan gspread
 client = gspread.authorize(creds)
